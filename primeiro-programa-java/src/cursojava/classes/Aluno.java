c74608feecb302cd7d619500a6627eea4f2132b3
vallzin <valmilson@gmail.com>
2025-02-22 11:59:06 -0300

    atributo disciplina

--- primeiro-programa-java/src/cursojava/classes/Aluno.java
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -1,5 +1,7 @@
 package cursojava.classes;
 
+import java.util.Objects;
+
 public class Aluno {
 	
 	private String nome;
@@ -13,10 +15,15 @@ public class Aluno {
 	private String nomeEscola;
 	private String serieMatriculado;
 	
-	private double nota1;
-	private double nota2;
-	private double nota3;
-	private double nota4;
+	private Disciplina disciplina = new Disciplina();
+	
+	public void setDisciplina(Disciplina disciplina) {
+		this.disciplina = disciplina;
+	}
+	
+	public Disciplina getDisciplina() {
+		return disciplina;
+	}
 	
 	public Aluno() {
 	}
@@ -110,40 +117,11 @@ public class Aluno {
 		this.serieMatriculado = serieMatriculado;
 	}
 
-	public double getNota1() {
-		return nota1;
-	}
-
-	public void setNota1(double nota1) {
-		this.nota1 = nota1;
-	}
-
-	public double getNota2() {
-		return nota2;
-	}
-
-	public void setNota2(double nota2) {
-		this.nota2 = nota2;
-	}
-
-	public double getNota3() {
-		return nota3;
-	}
-
-	public void setNota3(double nota3) {
-		this.nota3 = nota3;
-	}
-
-	public double getNota4() {
-		return nota4;
-	}
-
-	public void setNota4(double nota4) {
-		this.nota4 = nota4;
-	}
-	
 	public double getMediaNota() {
-		return (nota1 + nota2 + nota3 + nota4)/4;
+		return (disciplina.getNota1() + 
+				disciplina.getNota3() + 
+				disciplina.getNota3() + 
+				disciplina.getNota4())/4;
 	}
 	
 	public boolean getAlunoAprovado() {
@@ -163,5 +141,64 @@ public class Aluno {
 			return "Aluno está reprovado";
 		}
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dataMatricula, dataNascimento, idade, nome, nomeEscola, nomeMae, nomePai, numeroCPF,
+				registroGeral, serieMatriculado);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		return Objects.equals(dataMatricula, other.dataMatricula)
+				&& Objects.equals(dataNascimento, other.dataNascimento) && idade == other.idade
+				&& Objects.equals(nome, other.nome) && Objects.equals(nomeEscola, other.nomeEscola)
+				&& Objects.equals(nomeMae, other.nomeMae) && Objects.equals(nomePai, other.nomePai)
+				&& Objects.equals(numeroCPF, other.numeroCPF) && Objects.equals(registroGeral, other.registroGeral)
+				&& Objects.equals(serieMatriculado, other.serieMatriculado);
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
+				+ registroGeral + ", numeroCPF=" + numeroCPF + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai
+				+ ", dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola + ", serieMatriculado="
+				+ serieMatriculado + ", disciplina=" + disciplina + "]";
+	}
+
+	/*
+	@Override
+	public String toString() {
+		return "Aluno nome =" + this.getNome() + "\n" +
+				"idade =" + this.getIdade() + "\n" + 
+				"data de Nascimento =" + this.getDataNascimento() + "\n" + 
+				"registro Geral =" + this.getRegistroGeral() + "\n" + 
+				"CPF  =" + this.getNumeroCPF() + "\n" + 
+				"nome da Mae =" + this.getNomeMae() + "\n" + 
+				"nome do Pai =" + this.getNomePai() + "\n" + 
+				"data de Matricula =" + this.getDataMatricula() + "\n" + 
+				"nome da Escola =" + this.getNomeEscola() + "\n" + 
+				"serie Matriculado =" + this.getSerieMatriculado() + "\n" + 
+				"disciplina 1 =" + disciplina.getDisciplina1() + "\n" + 
+				"nota 1 =" + disciplina.getNota1() + "\n" + 
+				"disciplina 2 =" + disciplina.getDisciplina2() + "\n" + 
+				"nota 2 =" + disciplina.getNota2() + "\n" + 
+				"disciplina 3 =" + disciplina.getDisciplina3() + "\n" +
+				"nota 3 =" + disciplina.getNota3() + "\n" +
+				"disciplina 4 =" + disciplina.getDisciplina4() + "\n" + 
+				"nota 4 =" + disciplina.getNota4() + "\n" +
+				"Média do aluno = " + getMediaNota() +  "\n" +
+				"Situação do aluno = " + getAlunoAprovado2() + "\n"; 
+				
+	}*/
+	
+	
 	
 }
--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -18,9 +18,14 @@ public class PrimeiraClasseJava {
 		String dataMatricula = JOptionPane.showInputDialog("Data da matricula?");
 		String nomeEscola = JOptionPane.showInputDialog("Nome da escola?");
 		String serieMatriculada = JOptionPane.showInputDialog("Série matriculada?");
+		
+		String disciplina1 = JOptionPane.showInputDialog("Disciplina 1?");
 		String nota1 = JOptionPane.showInputDialog("Nota 1?");
+		String disciplina2 = JOptionPane.showInputDialog("Disciplina 2?");
 		String nota2 = JOptionPane.showInputDialog("Nota 2?");
+		String disciplina3 = JOptionPane.showInputDialog("Disciplina 3?");
 		String nota3 = JOptionPane.showInputDialog("Nota 3?");
+		String disciplina4 = JOptionPane.showInputDialog("Disciplina 4?");
 		String nota4 = JOptionPane.showInputDialog("Nota 4?");
 		
 		Aluno aluno1 = new Aluno();//Aqui será o João
@@ -34,11 +39,16 @@ public class PrimeiraClasseJava {
 		aluno1.setDataMatricula(dataMatricula);
 		aluno1.setNomeEscola(nomeEscola);
 		aluno1.setSerieMatriculado(serieMatriculada);
-		aluno1.setNota1(Integer.parseInt(nota1));
-		aluno1.setNota2(Integer.parseInt(nota2));
-		aluno1.setNota3(Integer.parseInt(nota3));
-		aluno1.setNota4(Integer.parseInt(nota4));
+		aluno1.getDisciplina().setDisciplina1(disciplina1);
+		aluno1.getDisciplina().setNota1(Double.parseDouble(nota1));
+		aluno1.getDisciplina().setDisciplina2(disciplina2);
+		aluno1.getDisciplina().setNota2(Double.parseDouble(nota2));
+		aluno1.getDisciplina().setDisciplina3(disciplina3);
+		aluno1.getDisciplina().setNota3(Double.parseDouble(nota3));
+		aluno1.getDisciplina().setDisciplina4(disciplina4);
+		aluno1.getDisciplina().setNota4(Double.parseDouble(nota4));
 		
+		/*
 		System.out.println("Nome do aluno: " + aluno1.getNome());
 		System.out.println("Idade é = " + aluno1.getIdade());
 		System.out.println("Nascimento = " + aluno1.getDataNascimento());
@@ -47,13 +57,9 @@ public class PrimeiraClasseJava {
 		System.out.println("Resultado2 = " + aluno1.getAlunoAprovado2());
 		/*-------------------------------------------------------------------*/
 		
-		Aluno aluno2 = new Aluno();//Aqui será o Pedro
+		Aluno aluno = new Aluno();
+		System.out.println(aluno1.toString());
 		
-		Aluno aluno3 = new Aluno();//Aqui será o Valval
-		
-		Aluno aluno4 = new Aluno("Maria");
-		
-		Aluno aluno5 = new Aluno("José", 50);
 
 	}
 
